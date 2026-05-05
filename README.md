0067e9115ea762b56731e6449971ce781cadc6ff
Donthu Avinash Babu <avinash4216@gmail.com>
2026-05-05 19:37:07 +0530

    python basics

---
+++ README.md
@@ -0,0 +1,2 @@
+# Welcome to Book
+* [Table of Contents](book/README.md)
---
+++ push.bat
@@ -0,0 +1,30 @@
+@echo off
+set HTTPS_PROXY=
+echo -----------------------------------------------
+cd /d %cd%
+echo echo Present working directory : %cd%
+echo -----------------------------------------------
+echo Pulling latest commits before push
+git pull
+echo -----------------------------------------------
+echo Checking status:
+git status
+echo -----------------------------------------------
+echo Adding files to staging area
+git add .
+echo Added to staging area
+echo -----------------------------------------------
+echo Checking status:
+git status
+echo -----------------------------------------------
+echo Committing:
+set /p commitMessage=Enter Commit Message: 
+git commit -m "%commitMessage%"
+::git commit -m "java"
+echo Committed
+echo -----------------------------------------------
+echo Pushing:
+git push
+echo Pushed
+echo -----------------------------------------------
+pause
